8ecacf08dae82d6af831b9b403026171743c575d
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-09-28 20:26:32 -0500

    Added verifyToken middleware

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,69 +3,61 @@ const router = Router();
 
 const jwt = require("jsonwebtoken");
 const config = require("../config");
+const verifyToken = require("./verifyToken");
 
 const User = require("../models/User");
 
 router.post("/signup", async (req, res, next) => {
-    const { username, email, password } = req.body;
-    const user = new User({
-        username, email, password
-    });
+  const { username, email, password } = req.body;
+  const user = new User({
+    username,
+    email,
+    password
+  });
 
-    user.password = await user.encryptPassword(user.password);
-    await user.save();
+  user.password = await user.encryptPassword(user.password);
+  await user.save();
 
-    const token = jwt.sign({ id: user._id }, config.secret, {
-        expiresIn: 60 * 60 * 24
-    });
+  const token = jwt.sign({ id: user._id }, config.secret, {
+    expiresIn: 60 * 60 * 24
+  });
 
-    res.json({ auth: true, token });
+  res.json({ auth: true, token });
 });
 
 router.post("/signin", async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email: email });
+  const user = await User.findOne({ email: email });
 
-    if (!user) {
-        return res.status(404).send("The email doesn't exists");
-    }
+  if (!user) {
+    return res.status(404).send("The email doesn't exists");
+  }
 
-    const validPassword = await user.validatePassword(password);
+  const validPassword = await user.validatePassword(password);
 
-    if (!validPassword) {
-        return res.status(401).json({
-            auth: false,
-            token: null
-        });
-    }
-
-    const token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 60 * 60 * 24
+  if (!validPassword) {
+    return res.status(401).json({
+      auth: false,
+      token: null
     });
+  }
 
-    return res.json({ auth: true, token });
+  const token = jwt.sign({ id: user.id }, config.secret, {
+    expiresIn: 60 * 60 * 24
+  });
+
+  return res.json({ auth: true, token });
 });
 
-router.get("/me", async (req, res, next) => {
-    const token = req.headers["x-access-token"];
+router.get("/me", verifyToken, async (req, res, next) => {
+  const user = await User.findById(req.userId, { password: 0 });
 
-    if (!token) {
-        return res.status(401).json({
-            auth: false,
-            message: "No token provided"
-        });
-    }
+  if (!user) {
+    return res.status(404).send("No user found");
+  }
 
-    const decoded = await jwt.verify(token, config.secret);
-
-    const user = await User.findById(decoded.id, { password: 0 });
-
-    if (!user) {
-        return res.status(404).send("No user found");
-    }
-
-    res.json(user);
+  res.json(user);
 });
 
 module.exports = router;
